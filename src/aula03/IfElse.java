268329d326ede5179ec453e46ae5c1ce9177c19b
RHU4N <rhuan3003@gmail.com>
2026-02-03 08:56:24 -0300

    if e else

---
+++ src/aula03/IfElse.java
@@ -0,0 +1,33 @@
+package aula03;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class IfElse {
+    static void main() {
+
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+
+//        int idade = 18;
+//        if (idade < 18){
+//            IO.println("Foi barrado na porta");
+//        }else {
+//            IO.println("Liberado");
+//        }
+        double nota = 0;
+        for (int i=1; i<=3;i++){
+            IO.println("Escreva a "+i+"° nota");
+            nota += sc.nextInt();
+        }
+        double media = nota/3;
+        if (media<4){
+            IO.println("Sua nota é "+df.format(media)+" e vc está reprovado");
+        } else if (media<=6) {
+            IO.println("Sua nota é "+df.format(media)+" e vc está de recuperação");
+        } else if (media>6) {
+            IO.println("Sua nota é "+df.format(media)+" e vc está aprovado");
+        }
+
+    }
+}
